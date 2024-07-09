1c63ce170418c8d0d57f8ff6a73bd9e34f547998
hmsmiraz <hmsmiraz64729@gmail.com>
2024-07-10 02:05:19 +0600

    correction

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -15,12 +15,16 @@ const auth = (...requiredRoles: TUserRole[]) => {
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
     }
-
+    let decoded;
     // checking if the given token is valid
-    const decoded = jwt.verify(
-      token,
-      config.jwt_access_secret as string,
-    ) as JwtPayload;
+    try {
+      decoded = jwt.verify(
+        token,
+        config.jwt_access_secret as string,
+      ) as JwtPayload;
+    } catch(err) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'Unauthorized!');
+    }
 
     const { role, userId, iat } = decoded;
 
